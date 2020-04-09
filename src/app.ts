beec6f7ea1c5393134161050c841448918d93f9c
Assasans <swimmin2@gmail.com>
2020-04-09 13:45:56 +0300

    User class, message classes, message history

--- src/app.ts
+++ src/app.ts
@@ -1,157 +1,18 @@
 import Logger, { registerLoggers } from './helper/logger';
 registerLoggers();
 
+import * as fs from 'promise-fs';
 import * as chalk from 'chalk';
 import * as http from 'http';
+import * as path from 'path';
 
 import { Gateway } from './websocket';
 
-const server: http.Server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
+const server: http.Server = http.createServer(async (request: http.IncomingMessage, response: http.ServerResponse) => {
 	Logger.web.trace(`[${chalk.green.bold(request.connection.remoteAddress)}]: ${chalk.yellow(request.method)} ${chalk.green(request.url)}`);
-	response.end(`
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8" />
-			<title>WebSocket Chat</title>
-		</head>
-		<body>
-			<style>
-			a {
-				font-family: Arial;
-			}
 
-			.message {
-				margin: 1em;
-				word-wrap: break-word;
-			}
-			.message-type {
-				font-weight: bold;
-			}
-
-			.message-content {
-				color: #212121;
-			}
-
-			.message-system {
-				color: #d32f2f;
-			}
-			.message-error {
-				color: #e65100;
-			}
-
-			.message-incoming {
-				color: #1565c0;
-			}
-			.message-outcoming {
-				color: #6a1b9a;
-			}
-			</style>
-			<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-			<script>
-
-			function _log(html) {
-				const chat = $('#chat');
-
-				const element = $('<span class="message"></span>');
-
-				element.html(html);
-
-				chat.append(element);
-				chat.append($('<br />'));
-			}
-
-			const log = {
-				system: function system(text) {
-					_log(\`<a class="message-type message-system">[Gateway]: </a><a class="message-content">\${text}</a>\`);
-				},
-				error: function error(text) {
-					_log(\`<a class="message-type message-error">[Error]: </a><a class="message-error">\${text}</a>\`);
-				},
-
-				incoming: function incoming(text) {
-					_log(\`<a class="message-type message-incoming">[Incoming]: </a><a class="message-incoming">\${text}</a>\`);
-				},
-				outcoming: function outcoming(text) {
-					_log(\`<a class="message-type message-outcoming">[Outcoming]: </a><a class="message-outcoming">\${text}</a>\`);
-				}
-			};
-
-			function sendMessage() {
-				const username = $('#input-username')[0].value;
-				const content = $('#input-content')[0].value;
-
-				$('#input-content')[0].value = '';
-
-				log.outcoming(\`[\${username || '<span style="color: #d84315;">Unknown</span>'}]: \${content}\`);
-				websocket.send(JSON.stringify({
-					username: username,
-					content: content
-				}));
-			}
-
-			function connect(serverURL) {
-				websocket = new WebSocket(serverURL);
-
-				websocket.onopen = (event) => {
-					log.system('Соединение с WebSocket сервером установлено');
-				};
-				websocket.onclose = (event) => {
-					log.system('Соединение с WebSocket сервером разорвано');
-				};
-				websocket.onmessage = (event) => {
-					try {
-						console.log(event.data);
-						const message = JSON.parse(event.data);
-						if(message.system) {
-							if(message.action === 'user_connect') {
-								log.system(\`Пациент <b>\${message.user}</b> попал в дурку\`);
-							}
-							if(message.action === 'user_disconnect') {
-								log.system(\`Пациент <b>\${message.user}</b> сбежал из дурки\`);
-							}
-						} else {
-							log.incoming(\`[\${message.username || '<span style="color: #d84315;">Unknown</span>'}]: \${message.content}\`);
-						}
-					} catch(error) {
-						log.error(error);
-					}
-				};
-				websocket.onerror = (event) => {
-					log.error(event.data);
-				};
-			}
-
-			$(document).ready(() => {
-				connect('ws://assasans.ml:2012/websocket');
-				
-				$('#button-send').on('click', (event) => {
-					sendMessage();
-				});
-				$(document).on('keypress', (event) => {
-					if(event.which === 13) {
-						sendMessage();
-					}
-				});
-
-				$('#button-chat-clear').on('click', (event) => {
-					$('#chat').empty();
-				});
-			});
-			</script>
-
-			<h3>Дурка</h3>
-
-			<input id="input-username" placeholder="Имя пользователя" /><br />
-			<input id="input-content" placeholder="Сообщение" /><br />
-			<button id="button-send">Отправить</button>
-			<button id="button-chat-clear">Очистить чат</button><br />
-			<br />
-
-			<div id="chat"></div>
-		</body>
-	</html>
-`);
+	response.setHeader('Access-Control-Allow-Origin', '*');
+	response.end(await fs.readFile(path.resolve('public', 'index.html')));
 });
 
 server.listen(2012, '0.0.0.0');
--- src/websocket.ts
+++ src/websocket.ts
@@ -3,10 +3,141 @@ import * as chalk from 'chalk';
 import * as http from 'http';
 import * as _ from 'lodash';
 
+import { DateTime } from 'luxon';
+
 import Logger from './helper/logger';
+
+export abstract class Message {
+	public time: DateTime;
+	public content: string;
+
+	constructor(time: DateTime, content: string) {
+		this.time = time;
+		this.content = content;
+	}
+}
+
+export type GatewayMessageType = 'custom' | 'user_connect' | 'user_disconnect';
+export class GatewayMessage extends Message {
+	public type: GatewayMessageType;
+
+	constructor(time: DateTime, content: string, type: GatewayMessageType) {
+		super(time, content);
+		this.type = type;
+	}
+
+	public toJSON() {
+		return {
+			type: 'gateway',
+			action: this.type,
+			content: this.content,
+			time: this.time.toFormat('HH:mm:ss')
+		}
+	}
+}
+
+export class GatewayCustomMessage extends GatewayMessage {
+	constructor(time: DateTime, message: string) {
+		super(time, message, 'custom');
+	}
+}
+
+export class UserConnectMessage extends GatewayMessage {
+	public user: User;
+	public ip: string;
+
+	constructor(time: DateTime, user: User, ip: string) {
+		super(time, `Пациент <b>${ip}</b> попал в дурку`, 'user_connect');
+
+		this.user = user;
+		this.ip = ip;
+	}
+
+	public toJSON() {
+		return {
+			type: 'gateway',
+			action: this.type,
+			content: this.content,
+			time: this.time.toFormat('HH:mm:ss'),
+			user: this.user,
+			ip: this.ip
+		}
+	}
+}
+
+export class UserDisconnectMessage extends GatewayMessage {
+	public user: User;
+	public ip: string;
+
+	constructor(time: DateTime, user: User, ip: string) {
+		super(time, `Пациент <b>${ip}</b> сбежал из дурки`, 'user_disconnect');
+
+		this.user = user;
+		this.ip = ip;
+	}
+
+	public toJSON() {
+		return {
+			type: 'gateway',
+			action: this.type,
+			content: this.content,
+			time: this.time.toFormat('HH:mm:ss'),
+			user: this.user,
+			ip: this.ip,
+		}
+	}
+}
+
+export class UserMessage extends Message {
+	public user: User;
+	public username: string | null;
+
+	constructor(time: DateTime, user: User, username: string | null, content: string) {
+		super(time, content);
+		this.user = user;
+		this.username = username;
+	}
+
+	public toJSON() {
+		return {
+			type: 'user',
+			username: this.username,
+			content: this.content,
+			time: this.time.toFormat('HH:mm:ss'),
+			user: this.user
+		}
+	}
+}
+
+export class User {
+	public id: number;
+	public messages: Message[];
+	
+	public socket: WebSocket;
+
+	constructor(id: number, messages: Message[], socket: WebSocket) {
+		this.id = id;
+		this.messages = messages;
+
+		this.socket = socket;
+	}
+
+	public toJSON() {
+		return {
+			id: this.id
+		};
+	}
+
+	public disconnect(): void {
+		this.socket.close();
+	}
+}
 
 export class Gateway {
 	public server: WebSocket.Server;
+
+	public users: User[];
+	public messages: Message[];
 
 	constructor(server: http.Server, path: string) {
 		this.server = new WebSocket.Server({
@@ -14,35 +145,80 @@ export class Gateway {
 			path: path
 		});
 
+		this.users = [];
+		this.messages = [];
+
 		this.server.on('connection', (socket: WebSocket, request: http.IncomingMessage) => {
 			Logger.websocket.trace(`Connection: ${chalk.blueBright(request.connection.remoteAddress)}`);
-			this.broadcast(socket, JSON.stringify({
-				system: true,
-				action: `user_connect`,
-				user: request.connection.remoteAddress
-			}));
+
+			const userId: number = this.users.length + 1;
+			const user: User = new User(
+				userId,
+				_.filter(this.messages, (message: Message) => {
+					return message instanceof UserMessage && message.user.id === userId;
+				}),
+				socket
+			);
+			this.users.push(user);
+
+			_.each(this.messages, (message: Message) => {
+				socket.send(JSON.stringify(message));
+			});
+
+			const connectMessage: UserConnectMessage = new UserConnectMessage(
+				DateTime.local(),
+				user,
+				request.connection.remoteAddress || 'Unknown'
+			);
+			this.broadcastAll(JSON.stringify(connectMessage));
+			this.messages.push(connectMessage);
 
 			socket.on('close', (code: number, reason: string) => {
 				Logger.websocket.trace(`Connection ${chalk.blueBright(request.connection.remoteAddress)} closed with code ${chalk.blueBright(code)}`);
-				this.broadcast(socket, JSON.stringify({
-					system: true,
-					action: `user_disconnect`,
-					user: request.connection.remoteAddress
-				}));
+
+				const disconnectMessage: UserDisconnectMessage = new UserDisconnectMessage(
+					DateTime.local(),
+					user,
+					request.connection.remoteAddress || 'Unknown'
+				);
+				this.broadcastAll(JSON.stringify(disconnectMessage));
+				this.messages.push(disconnectMessage);
 			});
 
 			socket.on('message', (data: string) => {
 				Logger.websocket.trace(`Connection ${chalk.blueBright(request.connection.remoteAddress)} send message: ${chalk.blueBright(data)}`);
 				try {
 					const message = JSON.parse(data);
-					this.broadcast(socket, JSON.stringify({
-						username: message.username,
-						content: message.content
-					}));
+					if(message.system) {
+						const gatewayMessage: GatewayCustomMessage = new GatewayCustomMessage(
+							DateTime.local(),
+							message.content
+						);
+						this.broadcast(socket, JSON.stringify(gatewayMessage));
+						this.messages.push(gatewayMessage);
+					} else {
+						const userMessage: UserMessage = new UserMessage(
+							DateTime.local(),
+							user,
+							message.username || 'Unknown',
+							message.content
+						);
+						this.broadcast(socket, JSON.stringify(userMessage));
+						this.messages.push(userMessage);
+					}
 				} catch(error) {
-					socket.send('Invalid JSON!');
+					socket.send(JSON.stringify(new GatewayCustomMessage(
+						DateTime.local(),
+						'Invalid JSON!'
+					)));
 				}
 			});
+		});
+	}
+
+	private broadcastAll(content: string): void {
+		this.server.clients.forEach((client: WebSocket) => {
+			client.send(content);
 		});
 	}
 
